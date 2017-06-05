fa6b23c841badf2cc8cd3a4f5a6fb6aaec925b7e
Alla Sidko <allochka.sidko@gmail.com>
2017-06-05 18:11:03 +0300

    Initial project. Simple bot

---
+++ app.js
@@ -0,0 +1,137 @@
+var Botkit = require('botkit')
+
+var token = 'xoxb-192818834532-DrpYX4YU71UC0ZcKZMO4QsDP'
+
+var controller = Botkit.slackbot({
+  // reconnect to Slack RTM when connection goes bad
+  retry: Infinity,
+  debug: false
+})
+
+// Assume single team mode if we have a SLACK_TOKEN
+if (token) {
+  console.log('Starting in single-team mode')
+  controller.spawn({
+    token: token
+  }).startRTM(function (err, bot, payload) {
+    if (err) {
+      throw new Error(err)
+    }
+
+    console.log('Connected to Slack RTM')
+  })
+// Otherwise assume multi-team mode - setup beep boop resourcer connection
+} else {
+  console.log('Starting in Beep Boop multi-team mode')
+  require('beepboop-botkit').start(controller, { debug: true })
+}
+
+controller.on('bot_channel_join', function (bot, message) {
+  bot.reply(message, "I'm here!")
+})
+
+controller.hears(['hi'], ['ambient', 'direct_message','direct_mention','mention'], function (bot, message) {
+  bot.reply(message, 'Hello.')
+})
+
+controller.hears(
+  ['hello', 'hi'], ['direct_message', 'direct_mention', 'mention'],
+  function (bot, message) { bot.reply(message, 'Meow. :smile_cat:') })
+
+// START: listen for cat emoji delivery
+var maxCats = 20
+var catEmojis = [
+  ':smile_cat:',
+  ':smiley_cat:',
+  ':joy_cat:',
+  ':heart_eyes_cat:',
+  ':smirk_cat:',
+  ':kissing_cat:',
+  ':scream_cat:',
+  ':crying_cat_face:',
+  ':pouting_cat:',
+  ':cat:',
+  ':cat2:',
+  ':leopard:',
+  ':lion_face:',
+  ':tiger:',
+  ':tiger2:'
+]
+
+controller.hears(
+  ['cat', 'cats', 'kitten', 'kittens'],
+  ['ambient', 'direct_message', 'direct_mention', 'mention'],
+  function (bot, message) {
+    bot.startConversation(message, function (err, convo) {
+      if (err) {
+        console.log(err)
+        return
+      }
+      convo.ask('Does someone need a kitten delivery? Say YES or NO.', [
+        {
+          pattern: bot.utterances.yes,
+          callback: function (response, convo) {
+            convo.say('Great!')
+            convo.ask('How many?', [
+              {
+                pattern: '[0-9]+',
+                callback: function (response, convo) {
+                  var numCats =
+                  parseInt(response.text.replace(/[^0-9]/g, ''), 10)
+                  if (numCats === 0) {
+                    convo.say({
+                      'text': 'Sorry to hear you want zero kittens. ' +
+                        'Here is a dog, instead. :dog:',
+                      'attachments': [
+                        {
+                          'fallback': 'Chihuahua Bubbles - https://youtu.be/s84dBopsIe4',
+                          'text': '<https://youtu.be/s84dBopsIe4|' +
+                            'Chihuahua Bubbles>!'
+                        }
+                      ]
+                    })
+                  } else if (numCats > maxCats) {
+                    convo.say('Sorry, ' + numCats + ' is too many cats.')
+                  } else {
+                    var catMessage = ''
+                    for (var i = 0; i < numCats; i++) {
+                      catMessage = catMessage +
+                      catEmojis[Math.floor(Math.random() * catEmojis.length)]
+                    }
+                    convo.say(catMessage)
+                  }
+                  convo.next()
+                }
+              },
+              {
+                default: true,
+                callback: function (response, convo) {
+                  convo.say(
+                    "Sorry, I didn't understand that. Enter a number, please.")
+                  convo.repeat()
+                  convo.next()
+                }
+              }
+            ])
+            convo.next()
+          }
+        },
+        {
+          pattern: bot.utterances.no,
+          callback: function (response, convo) {
+            convo.say('Perhaps later.')
+            convo.next()
+          }
+        },
+        {
+          default: true,
+          callback: function (response, convo) {
+            // Repeat the question.
+            convo.repeat()
+            convo.next()
+          }
+        }
+      ])
+    })
+  })
+  // END: listen for cat emoji delivery
