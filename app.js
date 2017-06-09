80b8a3ed8f6f1278e4efce2772b046be13cf2fec
Alla Sidko <allochka.sidko@gmail.com>
2017-06-09 23:18:23 +0300

    added notifications every minutes

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 var Botkit = require('botkit')
 
-var token = 'xoxb-192818834532-DrpYX4YU71UC0ZcKZMO4QsDP'
 
+var token = 'xoxb-192818834532-DrpYX4YU71UC0ZcKZMO4QsDP'
+//var port = process.env.PORT || 5000;
 var controller = Botkit.slackbot({
   // reconnect to Slack RTM when connection goes bad
   retry: Infinity,
   debug: false
 })
 
+var cron = require('node-cron');
+ 
+
 // Assume single team mode if we have a SLACK_TOKEN
-if (token) {
-  console.log('Starting in single-team mode')
-  controller.spawn({
+//if (token) {
+ // console.log('Starting in single-team mode')
+ var bot =  controller.spawn({
     token: token
   }).startRTM(function (err, bot, payload) {
     if (err) {
@@ -21,21 +25,64 @@ if (token) {
     console.log('Connected to Slack RTM')
   })
 // Otherwise assume multi-team mode - setup beep boop resourcer connection
-} else {
-  console.log('Starting in Beep Boop multi-team mode')
-  require('beepboop-botkit').start(controller, { debug: true })
-}
+//} else {
+//  console.log('Starting in Beep Boop multi-team mode')
+ // require('beepboop-botkit').start(controller, { debug: true })
+//}
 
-//app.get('/', function(req,res){res.status(200).send('Hello world'!)})
 
 controller.on('bot_channel_join', function (bot, message) {
   bot.reply(message, "I'm here!")
 })
 
+
+
+var x =  function (bot, message) {
+  bot.say(
+  {
+    text: 'my message text',
+    channel: 'C5K9XRGQ4' // a valid slack channel, group, mpim, or im ID
+  })
+}
+
+//1995, 11, 17, 3, 24, 0
+
+//var date = new  Date(2017,9,6,21,6);
+cron.schedule('* * * * *', function(){
+  //var now = new Date();
+  //var now2 = new Date(now.getFullYear,now.getMonth, now.getDay, now.getHours, now.getMinutes)
+
+  //if(now.getFullYear == date.getFullYear&&now.getMonth == date.getMonth&&now.getDay == date.getDay&&now.getHours == date.getHours&&now.getMinutes == date.getMinutes)
+  console.log('Every minute');
+  //else console.log('Check in every minutes');
+ 
+ bot.api.im.open({
+        user: 'C5K9XRGQ4'
+    }, (err, res) => {
+        if (err) {
+            bot.botkit.log('Failed to open IM with user', err)
+        }
+        console.log(res);
+        bot.startConversation({
+            user: 'C5K9XRGQ4',
+            channel: 'C5K9XRGQ4',
+            text: 'WOWZA... 1....2'
+        }, (err, convo) => {
+            convo.say('This is the shit')
+        });
+    })
+
+
+// Send the user who added the bot to their team a welcome message the first time it's connected
+
+ 
+});
+
+var now = new Date();
 controller.hears(
   ['hello', 'hi', 'halo'], ['direct_message', 'direct_mention', 'mention'],
-  function (bot, message) { bot.reply(message, 'Meow. :smile_cat:') })
-
+  function (bot, message) { bot.reply(message, 'Current date = ' + now) })
+/*
 // START: listen for cat emoji delivery
 var maxCats = 20
 var catEmojis = [
@@ -133,3 +180,4 @@ controller.hears(
     })
   })
   // END: listen for cat emoji delivery
+  */
